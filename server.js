02c744653565d03d383cdcfb423afde2a7b0c267
Enowtabi <enowbibi@gmail.com>
2025-06-08 16:06:16 +0100

    forth commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI||"mongodb+srv://enowbibi:HS4H5UefuclbBsl7@cluster0.py2y2ck.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
